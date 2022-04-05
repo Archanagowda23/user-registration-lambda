0a7cc945695c3ea3ebd5b1601bc5ab1e91eb063d
Archanagowda23 <archanar249@gmail.com>
2022-04-05 08:11:44 +0530

    added welcome message

---
+++ src/com/bridgelabz/algorithmprograms/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.algorithmprograms;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+	System.out.println("Welcome to lambda user-registation program");
+    }
+}
